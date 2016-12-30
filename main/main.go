eb416b33d2c7798ec4f01df745dd0b634acd5bf4
viile <dakgzhu@foxmail.com>
2016-12-30 16:39:03 +0800

    add test

--- main/main.go
+++ main/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"singleton"
+	"factory"
+	"factoryAbstract"
 )
 
 func main() {
+	fmt.Println("start")
 	mSingleton, nSingleton := singleton.NewSingleton("hello"), singleton.NewSingleton("hi")
 	mSingleton.DoSomething()
 	nSingleton.DoSomething()
+
+	var fac factory.CarFactory
+	car := fac.CreateCar("benz")
+	car.Create("benz 3")
+	car.Drive()
+
+	c := new(factoryAbstract.ProgrammerCreator)
+	p := c.Create()
+	taskProject := "project"
+	p.Produce(&taskProject)
 }
